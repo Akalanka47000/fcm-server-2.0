79f7b69006a85d21f43c71c45abdb3fa5f9fac4d
Akalanka <akalankaperera128@gmail.com>
2023-01-24 19:26:22 +0530

    Removed debug line

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,5 @@
 module.exports = (req, res, next) => {
     if (!req.headers.authorization) return res.status(401).send("Access denied. No token provided.");
-    res.send(process.env.SLIIT_EATS_SERVER_AUTH)
     if (req.headers.authorization !== process.env.SLIIT_EATS_SERVER_AUTH) return res.status(401).send("Access denied. Invalid token.");
     next();
 }
