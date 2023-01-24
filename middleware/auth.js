1e1c8169b44fa916379177fefd942a490d4fa6e0
Akalanka <akalankaperera128@gmail.com>
2023-01-24 19:36:48 +0530

    Minor fixes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,15 @@
-module.exports = (req, res, next) => {
+const authorize = (req, res, next) => {
     if (!req.headers.authorization) return res.status(401).send("Access denied. No token provided.");
     if (req.headers.authorization !== process.env.SLIIT_EATS_SERVER_AUTH) return res.status(401).send("Access denied. Invalid token.");
     next();
+}
+
+const authorizeV2 = (req, res, next) => {
+    if (req.headers.authorizationv2 !== process.env.SLIIT_EATS_SERVER_AUTH_TOKEN_2) return res.status(401).send("Access denied. Invalid token.");
+    next();
+}
+
+module.exports = {
+    authorize,
+    authorizeV2
 }
